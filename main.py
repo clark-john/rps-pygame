2245b31d2d7cc0415d56579f453d0da21bc84aa7
RhandyPiedadMartinez <mrpmartinez@tip.edu.ph>
2022-05-01 13:57:47 +0800

    functional gameplay added more spaghetti code, but working added image sprites added functional physics and attack sprite update
    Change-Id: Ib5343fdb29580c7a421f32f658304181cf1aa2d0
    Signed-off-by: RhandyPiedadMartinez <mrpmartinez@tip.edu.ph>

--- main.py
+++ main.py
@@ -34,19 +34,16 @@ player1 = Player(
 )
 player2 = Player(
     "darkmagenta",
-    attack.AttackType.ROCK,
+    attack.AttackType.SCISSORS,
     Controller(left=K_a, right=K_d, up=K_w, down=K_s),
     (177,100)
 )
 ground1 = Ground()
 
-#player2.rect.move_ip(177, 100)
-#player1.rect.move_ip(599, 100)
-
 running = True
 
 while running:
-    screen.fill((255, 255, 255))
+    screen.fill((255, 255, 128, 255))
     screen.blit(ground1.surf, ground1.rect)
 
     for event in pygame.event.get():
@@ -75,76 +72,72 @@ while running:
     if pygame.sprite.collide_rect(player1, player2): 
         if (player1.atk == player2.atk):
             if sp1 == 0:
-                player2.speed.x = -sp2 * 2
+                player2.speed.x = -sp2 * 3
                 player1.speed.x = -player2.speed.x
             else:
-                player1.speed.x = -sp1 * 2
+                player1.speed.x = -sp1 * 3
                 player2.speed.x = -player1.speed.x
         else:
             if (player1.atk == 1 and player2.atk == 3) \
             or (player1.atk == 2 and player2.atk == 1) \
             or (player1.atk == 3 and player2.atk == 2):
                 if sp1 == 0:
-                    player2.speed.x = -sp2 * 2
+                    player2.speed.x = -sp2 * 3
                     player1.speed.x = 0
                 else:
-                    player2.speed.x = sp1 * 2
+                    player2.speed.x = sp1 * 3
                     player1.speed.x = 0
             else:
                 if sp1 == 0:
-                    player1.speed.x = sp2 * 2
+                    player1.speed.x = sp2 * 3
                     player2.speed.x = 0
                 else:
-                    player1.speed.x = -sp1 * 2
+                    player1.speed.x = -sp1 * 3
                     player2.speed.x = 0
 
         #      r p s
         #    r x l w
         #    p w x l
         #    s l w x
-        
-        # <rect(177, 400, 50, 50)>
-        # <rect(594, 400, 50, 50)>    
-        # player1.gravity()
-        # player2.gravity()
-            
-        # # New gravity realistic
+ 
     if player1.speed.y < 0:
-        if (not pygame.sprite.collide_rect(player1, player2)):
-            if (player1.rect.top <= 150):
-                player1.speed.y = 0
+        if pygame.sprite.collide_rect(player1, player2):
+            if not (pygame.sprite.collide_rect(player1, ground1) or pygame.sprite.collide_rect(player2, ground1)):
+                player1.speed.y += 3
         else: 
-            player1.speed.y += 3
+            if (player1.rect.top <= 100):
+                player1.speed.y = 0
     else:
         if pygame.sprite.collide_rect(player1, ground1):  # checks collision player to ground
             player1.speed.y = 0
             temp1 = player1.allowjump
             player1.allowjump = True
             if (temp1 != player1.allowjump):
-                print('player1: ' + str(player1.atk_stats()))
+                #print('player1: ' + str(player1.atk_stats()))
                 player1.change_atk()
         else:
             player1.speed.y += 0.02
-            print(str(player1.speed))
+            #print(str(player1.speed))
             player1.allowjump = False
     
     if player2.speed.y < 0:
-        if (not pygame.sprite.collide_rect(player1, player2)):
-            if (player2.rect.top <= 150):
-                player2.speed.y = 0
+        if pygame.sprite.collide_rect(player1, player2):
+            if not (pygame.sprite.collide_rect(player1, ground1) or pygame.sprite.collide_rect(player2, ground1)):
+                player2.speed.y += 3
         else: 
-            player2.speed.y += 3
+            if (player2.rect.top <= 100):
+                player2.speed.y = 0
     else:
         if pygame.sprite.collide_rect(player2, ground1):  # checks collision player to ground
             player2.speed.y = 0
             temp2 = player2.allowjump
             player2.allowjump = True
             if (temp2 != player2.allowjump):
-                print('player2: ' + str(player2.atk_stats()))
+                #print('player2: ' + str(player2.atk_stats()))
                 player2.change_atk()
         else:
             player2.speed.y += 0.02
-            print(str(player2.speed))
+            #print(str(player2.speed))
             player2.allowjump = False
     
     # disables player flying upwards when below ground
@@ -166,9 +159,11 @@ while running:
     player2.move()
     player1.move()
     
-    screen.blit(player1.surf, player1.rect)
-    screen.blit(player2.surf, player2.rect)
-
+    
+    #screen.blit(player1.surf, player1.rect)
+    #screen.blit(player2.surf, player2.rect)
+    screen.blit(player1.image, player1.rect)
+    screen.blit(player2.image, player2.rect)
     pygame.display.flip()
 
 pygame.quit()
--- player/player.py
+++ player/player.py
@@ -7,7 +7,9 @@ from tkinter import messagebox as msg
 from core.attack import AttackType
 from core.constants import SCREEN_HEIGHT,SCREEN_WIDTH
 from controller.controller import Controller
+from pygame.image import load
 import os
+
 class Player(Sprite):
 
     def __init__(self, sprite_color, atk:AttackType, controller:Controller, init_pos):
@@ -22,13 +24,25 @@ class Player(Sprite):
                 return thiscolor
             else:
                 return thiscolor
-
+                
         super(Player, self).__init__()
         self.controller = controller
+        """ 
         self.surf = Surface((50, 50))
         thiscolor = name_to_rgb(sprite_color)
         self.surf.fill(thiscolor)
-        self.rect = self.surf.get_rect()
+        self.rect = self.surf.get_rect() 
+        """
+
+        self.image = pygame.image.load("./assets/scissors.png").convert()
+        
+        # return a width and height of an image
+        self.size = self.image.get_size()
+        
+        # create a smaller image than self.image
+        self.image = pygame.transform.scale(self.image, (int(self.size[0]*0.3), int(self.size[1]*0.3)))
+        self.rect = self.image.get_rect() 
+
         self.atk = atk
         self.allowjump = False
         self.speed = pygame.Vector2()
@@ -44,8 +58,8 @@ class Player(Sprite):
             self.speed.x = 1
          
         if pressed_keys[self.controller.up] and self.allowjump == True:
-            self.speed.y += -0.2
-                
+            self.speed.y -= 0.5
+
         if pressed_keys[self.controller.down]:
             self.speed.x = 0
   
@@ -59,13 +73,12 @@ class Player(Sprite):
         
     def bound(self):
         if self.rect.right > SCREEN_WIDTH:
-            self.rect.right = SCREEN_WIDTH
+            self.speed.x -= 0.3
 
         if self.rect.top <= 0:
             self.rect.top = 0
 
         if self.rect.bottom >= SCREEN_HEIGHT:
-            self.speed.y = 0
 
             print("bottom of the screen")
             restart = msg.askyesno("rps-pygame","Play game again?")
@@ -75,7 +88,7 @@ class Player(Sprite):
                 exit()
                 
         if self.rect.left < 0:
-            self.rect.left = 0
+            self.speed.x += 0.3
     
     def atk_stats(self): 
         if self.atk == AttackType.ROCK:
@@ -90,3 +103,28 @@ class Player(Sprite):
             self.atk = AttackType.ROCK
         else: 
             self.atk += 1
+
+        if self.atk == AttackType.ROCK:
+            self.image = pygame.image.load("./assets/rock.png").convert()
+            # return a width and height of an image
+            self.size = self.image.get_size()
+            
+            # create a 2x bigger image than self.image
+            self.image = pygame.transform.scale(self.image, (int(self.size[0]*0.3), int(self.size[1]*0.3)))
+       
+        elif self.atk == AttackType.PAPER:
+            self.image = pygame.image.load("./assets/paper.png").convert()
+            # return a width and height of an image
+            self.size = self.image.get_size()
+            
+            # create a 2x bigger image than self.image
+            self.image = pygame.transform.scale(self.image, (int(self.size[0]*0.3), int(self.size[1]*0.3)))
+        
+        elif self.atk == AttackType.SCISSORS:
+            self.image = pygame.image.load("./assets/scissors.png").convert()
+            # return a width and height of an image
+            self.size = self.image.get_size()
+            
+            # create a 2x bigger image than self.image
+            self.image = pygame.transform.scale(self.image, (int(self.size[0]*0.3), int(self.size[1]*0.3)))
+            
